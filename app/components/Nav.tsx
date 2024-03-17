9f5205628c0446f5df44c7a5dddfba68fe3554df
NightBringerZied <benaliouchezied@gmail.com>
2024-03-17 16:11:46 +0100

    startin

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -72,7 +72,7 @@ const Nav = () => {
 
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="icon icon-tabler icon-tabler-user"
+          className="icon icon-tabler icon-tabler-user"
           width="44"
           height="44"
           viewBox="0 0 24 24"
