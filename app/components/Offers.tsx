72b0ed64f4883cd2e539b7dce49adca67ea3527a
alaasao <a_souchi@estin.dz>
2024-03-19 12:37:07 +0100

    new nav

--- app/components/Offers.tsx
+++ app/components/Offers.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Offers = () => {
   return (
-    <div className="pt-[200px] bg-white">
+    <div className="pt-[200px] bg-white" id="offers"> 
       <h1 className="w-[700px] text-center mt-[80px] max-lg:w-[300px] max-lg:text-3xl text-6xl text-black mx-auto font-medium">
         OFFRES DU MOMENT
       </h1>
