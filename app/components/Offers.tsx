07fd73e49ab12973d0a6b64c5e1d3c14f6b16a9d
alaasao <a_souchi@estin.dz>
2024-03-19 13:53:06 +0100

    footer

--- app/components/Offers.tsx
+++ app/components/Offers.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Offers = () => {
   return (
-    <div className="pt-[200px] bg-white" id="offers"> 
-      <h1 className="w-[700px] text-center mt-[80px] max-lg:w-[300px] max-lg:text-3xl text-6xl text-black mx-auto font-medium">
+    <div className="pt-[100px] bg-white" id="offers">
+      <h1 className="w-[700px] text-center  max-lg:w-[300px] max-lg:text-3xl text-6xl text-black mx-auto font-medium">
         OFFRES DU MOMENT
       </h1>
       <div className="w-full flex justify-between"></div>
