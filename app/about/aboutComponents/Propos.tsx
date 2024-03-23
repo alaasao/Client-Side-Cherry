c2e8da3fcf708eeef33305509fff86324dc43641
NightBringerZied <benaliouchezied@gmail.com>
2024-03-23 22:41:24 +0100

    nothing

--- app/about/aboutComponents/Propos.tsx
+++ app/about/aboutComponents/Propos.tsx
@@ -9,12 +9,12 @@ type Demoprops={
 const Propos = (props:Demoprops) => {
   const [collapse, setcollapse] = useState(false)
   return (
-    <div className='flex sm:ml-6 mt-6  flex-col justify-center'>
+    <div className='flex sm:ml-6 mt-6 flex-col justify-center'>
       <div className='good text-sm font-[400]  sm:text-2xl lg:text-4xl '>a propos de <span className='text-[#D12621]'>{props.title} :</span></div>
         <div className='flex sm:flex-row gap-4 mt-6 ml-6 '>
             <div className='w-[10px] h-auto bg-gradient-to-t from-[#D12621] to-[#6B1311] '>
             </div>
-          <div className=' sm:italic sm:max-w-[90%] w-full h-auto  text-sm  font-[400] sm:font-[500] sm:text-sm md:text-xl sm:text-start sm:pr-2 sm:leading-[37px]'>
+          <div className=' italic sm:max-w-[90%] w-full h-auto  text-sm  font-[500] sm:font-[500] sm:text-sm md:text-xl text-start sm:pr-2 sm:leading-[37px]'>
                                 Sarl Automobile Leader Company ALC est le représentant officiel de la marque CHERY en Algérie.<br/>
                     Depuis 2020, ALC est entrée en service en Algérie dans le but d&apos;assurer la distribution et la commercialisation des véhicules CHERY ainsi que, leur Service après-vente.<br/>
                     <span className={`max-h-0 transition-all ease-in-out duration-1000 overflow-hidden inline-block md:max-h-[50rem] ${collapse ? "max-h-[50rem] transition-all   ease-in-out duration-1000" :"" }` }>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,12 +7,12 @@ const page = () => {
 <div  className='h-[95%] w-screen flex items-center justify-center mb-[50px] bg-white text-black '>
     <div  className='w-[80%] flex flex-col justify-center items-center gap-8  '>
         <div className='w-full  h-[10%] flex  justify-center text-center bg-white text-black '>
-            <div className='w-[90%] h-[30%] mt-[5vh] sm:w-[80%] sm:h-[40vh] sm:mt-[5vh] shadow-3xl flex justify-around sm:justify-around  items-center  rounded-[10px]'>
+            <div className='w-[80%] h-[30vh]  mt-[5vh] sm:w-[80%] sm:h-[40vh] sm:mt-[5vh] shadow-3xl flex justify-around sm:justify-around  items-center  rounded-[10px]'>
                 <div>
-                    <div className='font-[500] text-wrap text-lg sm:text-3xl good leading-[54px] pb-2'>
+                    <div className='font-[500] text-wrap text-sm sm:text-2xl md:text-4xl good leading-[54px] pb-2'>
                         À propos de nous
                     </div>  
-                    <div  className='text-sm sm:text-xl sm:font-light font-light text-[#686565] pt-2 '>
+                    <div  className='text-xs sm:text-xl sm:font-light font-light text-[#686565] pt-2 '>
                         Apprenez plus sur nous
                     </div>
                 </div>
@@ -23,9 +23,9 @@ const page = () => {
         
         <div className='flex items-center justify-center'>
                 <Propos title='speed motors' description='Sarl Automobile Leader Company ALC est le représentant officiel de la marque CHERY en Algérie.<br/>
-                            Depuis 2020, ALC est entrée en service en Algérie dans le but d’assurer la distribution et la commercialisation des véhicules CHERY ainsi que, leur Service après-vente.<br/>
-                            Une équipe de cadres hautement qualifiée qui s’engage à fournir un produit représentatif et compétitif afin de satisfaire les clients et créer de l’emploi et de la richesse pour le pays.<br/>
-                            L’objectif de l’ALC est de proposer une large gamme de véhicules CHERY, couvrant les besoins essentiels du marché algérien avec un choix de gammes diverses, de segments, de motorisations et de finitions, sur pratiquement toutes les régions d’Algérie.<br/>
+                            Depuis 2020, ALC est entrée en service en Algérie dans le but d&apos;assurer la distribution et la commercialisation des véhicules CHERY ainsi que, leur Service après-vente.<br/>
+                            Une équipe de cadres hautement qualifiée qui s&apos;engage à fournir un produit représentatif et compétitif afin de satisfaire les clients et créer de l&apos;emploi et de la richesse pour le pays.<br/>
+                            L&apos;objectif de l&apos;ALC est de proposer une large gamme de véhicules CHERY, couvrant les besoins essentiels du marché algérien avec un choix de gammes diverses, de segments, de motorisations et de finitions, sur pratiquement toutes les régions d&apos;Algérie.<br/>
                             ALC compte prendre en charge tous les cas de garantie des véhicules qui seront vendus par son réseau en Algérie.<br/>
                             Ses succursales et agents agréés ne seront pas seulement des points de vente, mais elles offriront et couvriront également, tous les services et besoins des clients de la marque CHERY.'/>
                 </div>
