bebfa7e7df58e3affa3ba25c8f4b8951de67802c
yacine <a_zeraibi@estin.dz>
2024-04-02 13:59:41 +0100

    fixed building issue

--- app/services/servicesComponent/FirstSection.tsx
+++ app/services/servicesComponent/FirstSection.tsx
@@ -9,7 +9,7 @@ const FirstSection = () => {
           <div></div>
           <div className="flex justify-center items-center">
                 <div className='w-[80%] md:w-[90%] h-[10vh] sm:h-[25vh] flex justify-center items-center  bg-[#F7F7F7CF] '>
-                            <p className='p-5  text-sm sm:text-lg lg:text-2xl'><span className='text-[#EC0101]'>CHERY</span> - Votre partenaire de confiance pour l'entretien automobile </p>
+                            <p className='p-5  text-sm sm:text-lg lg:text-2xl'><span className='text-[#EC0101]'>CHERY</span> - Votre partenaire de confiance pour l&apos;entretien automobile </p>
                 </div>
           </div>
         </div>
