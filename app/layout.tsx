5a6444bedac81e34e9e43d1ee3fa9a1f998f7990
yacine <a_zeraibi@estin.dz>
2024-06-05 10:43:26 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ const poppins = Poppins({
   variable: "--font-mont",
   weight: "400",
 });
+
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Head from "next/head";
@@ -45,7 +46,7 @@ export default function RootLayout({
         <meta name="description" content="Lancé en septembre 2023, Chery Speed Motors privilégie une expérience d'achat de voiture simplifiée, en utilisant des solutions innovantes pour permettre aux clients de prendre des décisions éclairées. Engagé envers l'excellence, Chery Speed Motors vise à se positionner comme un leader dans l'industrie automobile en Algérie en pleine évolution." />
         <meta name="keywords" content="concessionnaire automobile, Chery Speed Motors, voitures neuves, voitures d'occasion, financement automobile, service de voitures, réparation de voitures, concessionnaire de voitures, vente de voitures, achat de voitures, certifié d'occasion, reprise de voiture, location de voiture, prêts automobiles" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="app/favicon.ico" />
       </Head>
       <body className={poppins.className}>
         <ScrollBar className={"relative ww overflow-x-hidden bg-white text-black"}>
